5907370c9415b595356031b221dfd55831d83aef
David Leon <jelid.leon@gmail.com>
2026-04-10 10:40:14 -0600

    Fix the envSecret2 to work with Vercel

--- src/helpers.js
+++ src/helpers.js
@@ -16,7 +16,7 @@ const templates = {};
 
 /* eslint-disable no-process-env */
 const envSecret1 = process.env.SLACK_VERIFICATION_TOKEN,
-      envSecret2 = process.env.MONGODB_URI;
+      envSecret2 = process.env.plusplus_MONGODB_URI || process.env.MONGODB_URI || 'mongodb://db:27017/plus';
 /* eslint-enable no-process-env */
 
 const ONE_DAY = 60 * 60 * 24, // eslint-disable-line no-magic-numbers
